8b34fc7c4c746473562b87af02761b66d4dc8bcb
Zennah Asumbrado <zenleasumbrado@gmail.com>
2023-10-02 15:18:35 +0800

    JPanel subs test 1

--- src/FramePositioning.java
+++ src/FramePositioning.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
 
 class FramePositioning extends JFrame{
-    public JPanel panelMain;
+    private JPanel mainPanel;
+    private JPanel subPanelA;
+    private JPanel subPanelB;
+
+    FramePositioning() {
+    }
 
     public static void main(String[] args) {
         FramePositioning frame = new FramePositioning();
-        JPanel panelSubA = new JPanel();
-        JPanel panelSubB = new JPanel();
-
-        panelSubA.setLocation(10,10);
-        panelSubB.setLocation(200, 90);
 
         frame.setTitle("Frame");
         frame.setSize(500,400);
