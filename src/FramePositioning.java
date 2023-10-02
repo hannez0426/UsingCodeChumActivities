524fe6dbec0f50260bef1c07adeb6e26951ae8d8
Zennah Asumbrado <zenleasumbrado@gmail.com>
2023-10-02 14:39:58 +0800

    JPanel subs

--- src/FramePositioning.java
+++ src/FramePositioning.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
 
-public class FramePositioning extends JFrame{
-    private JPanel panelMain;
+class FramePositioning extends JFrame{
+    public JPanel panelMain;
 
     public static void main(String[] args) {
         FramePositioning frame = new FramePositioning();
+        JPanel panelSubA = new JPanel();
+        JPanel panelSubB = new JPanel();
+
+        panelSubA.setLocation(10,10);
+        panelSubB.setLocation(200, 90);
+
         frame.setTitle("Frame");
         frame.setSize(500,400);
         frame.setVisible(true);
+
+
+
     }
 }
