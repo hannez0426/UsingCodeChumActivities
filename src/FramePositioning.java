1e9dde3be0d1b59f5622f9dbb21413a60a514e03
Zennah Asumbrado <zenleasumbrado@gmail.com>
2023-10-02 15:28:27 +0800

    JPanel subs test 2

--- src/FramePositioning.java
+++ src/FramePositioning.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 class FramePositioning extends JFrame{
     private JPanel mainPanel;
@@ -6,7 +7,21 @@ class FramePositioning extends JFrame{
     private JPanel subPanelB;
 
     FramePositioning() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null); // Set the layout manager of the main panel
+
+        JPanel subPanelA = new JPanel();
+        JPanel subPanelB = new JPanel();
+
+        subPanelA.setBounds(10, 10, 100, 100);
+        subPanelB.setBounds(200, 90, 100, 100);
+
+        mainPanel.add(subPanelA); // Add subPanelA to the main panel
+        mainPanel.add(subPanelB); // Add subPanelB to the main panel
+
+        add(mainPanel); // Add the main panel to the JFrame
     }
+
 
     public static void main(String[] args) {
         FramePositioning frame = new FramePositioning();
